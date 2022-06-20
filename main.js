51f3ff1e3cee5824945a21ae5932525f5a148c10
Leah <leinhorn@keplergrp.com>
2022-06-20 11:53:15 -0400

    Add wake word and support HELP, MEMBERS

--- main.js
+++ main.js
@@ -1,13 +1,43 @@
-exports.handler = (context, event, callback) => {
+const isWakeWord = (body) => {
+  return body.toLowerCase().startsWith('@einhorn');
+};
 
+const getMessage = (body) => {
+  switch (body.toLowerCase().trim()) {
+    case '@einhorn help':
+      const helpMessage = `
+      👋 Looks like you need some help. These are the commands I know about:
+
+      @Einhorn HELP: Get this message
+      @Einhorn MEMBERS: Get the list of members that are part of this group text
+
+    NOTE: This message was sent directly to you (only).
+    If there are other features you'd like to see, let me know!
+    `;
+      return helpMessage;
+    case '@einhorn members':
+      const phones = Object.keys(FAMILY_GROUP).map(
+        (phone) => `- ${FAMILY_GROUP[phone]}: ${phone} `
+      );
+      return `Members in this channel:\n${phones.join('\n')}`;
+    default:
+      return "Sorry, I don't understand that command.";
+  }
+};
+
+exports.handler = (context, event, callback) => {
   const twiml = new Twilio.twiml.MessagingResponse();
 
-  const fromNum = event.From.replace('+1', '')
+  const fromNum = event.From.replace('+1', '');
 
-  Object.keys(FAMILY_GROUP).forEach(phone => {
+  if (isWakeWord(event.Body)) {
+    const wakeMessage = getMessage(event.Body);
+    twiml.message({ to: fromNum }, wakeMessage);
+    return callback(null, twiml);
+  }
 
-    if(phone != fromNum) {
-
+  Object.keys(FAMILY_GROUP).forEach((phone) => {
+    if (phone != fromNum) {
       const message = twiml.message({ to: phone });
 
       message.body(`${FAMILY_GROUP[fromNum] || event.From}: ${event.Body}`);
@@ -16,6 +46,6 @@ exports.handler = (context, event, callback) => {
         message.media(event['MediaUrl' + i]);
       }
     }
-  })
+  });
   return callback(null, twiml);
 };
